b729d7d55ba20a96efbd826b31273c5fd2449562
amasalov <delsol@bk.ru>
2018-12-14 23:40:56 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ko from 'knockout';
+import '../node_modules/muicss/dist/css/mui.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const costumer = {
+  name: ko.observable('Гость'),
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const btn = document.querySelector('#nameButton');
+const input = document.querySelector('#nameInput');
+const mainForm = document.querySelector('#mainForm');
+
+mainForm.addEventListener('submit', (evt) => evt.preventDefault());
+
+btn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  costumer.name(input.value.trim());
+  input.value = '';
+});
+
+ko.applyBindings(costumer);
